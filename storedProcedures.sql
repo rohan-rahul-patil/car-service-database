6bf1073f21525bfff78801a3dbc62b502c69866b
mrunmayi96 <mrunmayi96@gmail.com>
2018-11-19 09:51:35 -0500

    procedures file re-organized

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -1,3 +1,6 @@
+
+/*_______________________________________________random_____________________________________________*/
+
 CREATE FUNCTION
 hello_world(hi in varchar)
 RETURN varchar
@@ -6,132 +9,6 @@ BEGIN
 	return(hi);
 END;
 
-
-
-CREATE OR REPLACE FUNCTION 
-authenticate_login( un in varchar2, pw in varchar2) 
-RETURN int
-AS
-x int := 0;
-begin 
-	select count(*) into x  from login_details where username=un and password = pw;
-	return(x);
-END authenticate_login;
-
-set serveroutput on
-begin
-dbms_ouput.put_line(authenticate_login('lovestory', 'lovestory123'));
-end;
-
-CREATE OR REPLACE FUNCTION
-get_user_details( un in varchar2, nm OUT varchar, ph OUT varchar, em OUT varchar)
-RETURN varchar2
-AS
-atype varchar2(50);
-userid int :=0;
-begin
-	select account_type into atype from login_details where username =  un;
-	select id into userid from login_details where username =  un;
-	if atype = 'manager' then
-		select name, phone_num, email into nm, ph, em from manager where employee_id = userid;
-	elsif atype = 'receptionist' then
-		select name, phone_num, email into nm, ph, em from receptionist where employee_id = userid;
-	elsif atype = 'customer' then 
-		select name, phone_num, email into nm, ph, em from customer where customer_id = userid;
-	end if;
-	return(atype);
-END get_user_details;
-
-
-CREATE OR REPLACE FUNCTION
-register_new_user(pw in varchar2, cid int, nm in varchar2, ad in varchar2, ph in varchar2, em in varchar2, sc varchar2)
-RETURN int
-AS
-x int := 0;
-y int := 0;
-begin
-	insert into login_details(username, password, account_type, id) values (em, pw, 'customer', cid);
-	insert into customer values(cid, nm, ad, ph, em,  sc);
-	select count(*) into x from login_details where username = em;
-	select count(*) into y from customer where customer_id = cid;
-	if (x+y = 2) then
-		return(1);
-	else
-		return(0);
-	end if;
-END register_new_user;
-
-CREATE OR REPLACE FUNCTION
-update_customer_profile(cid in int, nm in varchar, ad in varchar, ph in varchar, pw in varchar2)
-RETURN int
-AS
-x int:=0;
-begin
-	update customer set name = nm, address = ad, phone_num = ph where customer_id=cid;
-	update login_details set password = pw where id = cid;
-	x := 1;
-	return(x);
-end update_customer_profile;
-
-
-CREATE OR REPLACE FUNCTION
-registerCar(cid in int, lic in varchar2, p_date in date, mk in varchar2, md in varchar2, yr in int, miles in int, s_date in date)
-RETURN int
-AS
-x int := 0;
-y int := 0;
-begin
-	insert into vehicle (license_plate_num, make, model, year, purchase_date, last_mileage, date_of_last_service) values(lic, mk, md, yr, p_date, miles, s_date);
-	insert into owns values(lic, cid);
-	select count(*) into x from vehicle where license_plate_num = lic;
-	select count(*) into y from owns where license_plate_num = lic;
-	if (x+y = 2) then
-		return(1);
-	else
-		return(0);
-	end if;
-END registerCar;
-
-create or replace type appointment_record as object(
-	appointment_date_time timestamp,
-	service_type varchar2(100),
-	preferred_mechanic_id int,
-	license_plate_num varchar2(100)
-);
-
-create or replace type appointment_table as table of appointment_record;
-
-CREATE OR REPLACE FUNCTION
-view_service_history(cid in int)
-RETURN appointment_table 
-AS
-v_ret appointment_table;
-begin
-	select appointment_record(appointment_date_time, service_type, preferred_mechanic_id, license_plate_num) bulk collect into v_ret from appointment where customer_id = cid;
-	return(v_ret);
-end view_service_history;
-
-CREATE OR REPLACE FUNCTION
-schedule_maintainance_service(cid in int, lic in varchar, m in int, mech in varchar, d in timestamp)
-RETURN int
-AS
-mk varchar(100) ;
-md varchar(100);
-s_type varchar2(100);
-mech_id int;
-begin
-	select make, model into mk, md from vehicle where license_plate_num = lic;
-	select distinct max(service_type) into s_type from maintainance where vehicle_make = mk and vehicle_model = md and  miles <= m;
-	select employee_id  into mech_id from mechanic where name = mech;
-	insert into appointment values(d, s_type, null,  mech_id, lic, cid);
-	return(1);
-END schedule_maintainance_service;
-	
-<<<<<<< HEAD
-	
-=======
-
-/*____________________________________________________________________________________________________*/
 
 create or replace type login_record as object (
   username varchar2(50),
@@ -162,14 +39,67 @@ begin
 end get_login_details;
 /
 
-
-
 /*select *from table(get_login_details('lovestory', 'lovestory123'));*/
 
+/*__________________________________________________darshana_________________________________________________*/
 
-/*____________________________________________________________________________________________*/
+/*1*/
+CREATE OR REPLACE FUNCTION 
+authenticate_login( un in varchar2, pw in varchar2) 
+RETURN int
+AS
+x int := 0;
+begin 
+	select count(*) into x  from login_details where username=un and password = pw;
+	return(x);
+END authenticate_login;
+
+set serveroutput on
+begin
+dbms_ouput.put_line(authenticate_login('lovestory', 'lovestory123'));
+end;
 
 
+
+CREATE OR REPLACE FUNCTION
+get_user_details( un in varchar2, nm OUT varchar, ph OUT varchar, em OUT varchar)
+RETURN varchar2
+AS
+atype varchar2(50);
+userid int :=0;
+begin
+	select account_type into atype from login_details where username =  un;
+	select id into userid from login_details where username =  un;
+	if atype = 'manager' then
+		select name, phone_num, email into nm, ph, em from manager where employee_id = userid;
+	elsif atype = 'receptionist' then
+		select name, phone_num, email into nm, ph, em from receptionist where employee_id = userid;
+	elsif atype = 'customer' then 
+		select name, phone_num, email into nm, ph, em from customer where customer_id = userid;
+	end if;
+	return(atype);
+END get_user_details;
+
+/*2*/
+CREATE OR REPLACE FUNCTION
+register_new_user(pw in varchar2, cid int, nm in varchar2, ad in varchar2, ph in varchar2, em in varchar2, sc varchar2)
+RETURN int
+AS
+x int := 0;
+y int := 0;
+begin
+	insert into login_details(username, password, account_type, id) values (em, pw, 'customer', cid);
+	insert into customer values(cid, nm, ad, ph, em,  sc);
+	select count(*) into x from login_details where username = em;
+	select count(*) into y from customer where customer_id = cid;
+	if (x+y = 2) then
+		return(1);
+	else
+		return(0);
+	end if;
+END register_new_user;
+
+/*3*/
 create or replace type vehicle_record as object (
 	license_plate_num VARCHAR(100),
 	make VARCHAR(100),
@@ -184,7 +114,6 @@ create or replace type vehicle_record as object (
 
 create or replace type vehicle_table as table of vehicle_record;
 /
-
 
 create or replace function get_vehicles(p_customer_id varchar)
 return vehicle_table as
@@ -204,15 +133,80 @@ begin
 end get_vehicles;
 /
 
-
 /*select *from table(get_vehicles('1001'));*/
 
+/*4*/
+CREATE OR REPLACE FUNCTION
+update_customer_profile(cid in int, nm in varchar, ad in varchar, ph in varchar, pw in varchar2)
+RETURN int
+AS
+x int:=0;
+begin
+	update customer set name = nm, address = ad, phone_num = ph where customer_id=cid;
+	update login_details set password = pw where id = cid;
+	x := 1;
+	return(x);
+end update_customer_profile;
 
-/*___________________________________________________________________________________________________*/
+/*5*/
+CREATE OR REPLACE FUNCTION
+registerCar(cid in int, lic in varchar2, p_date in date, mk in varchar2, md in varchar2, yr in int, miles in int, s_date in date)
+RETURN int
+AS
+x int := 0;
+y int := 0;
+begin
+	insert into vehicle (license_plate_num, make, model, year, purchase_date, last_mileage, date_of_last_service) values(lic, mk, md, yr, p_date, miles, s_date);
+	insert into owns values(lic, cid);
+	select count(*) into x from vehicle where license_plate_num = lic;
+	select count(*) into y from owns where license_plate_num = lic;
+	if (x+y = 2) then
+		return(1);
+	else
+		return(0);
+	end if;
+END registerCar;
+
+create or replace type appointment_record as object(
+	appointment_date_time timestamp,
+	service_type varchar2(100),
+	preferred_mechanic_id int,
+	license_plate_num varchar2(100)
+);
+
+create or replace type appointment_table as table of appointment_record;
+/*6*/
+CREATE OR REPLACE FUNCTION
+view_service_history(cid in int)
+RETURN appointment_table 
+AS
+v_ret appointment_table;
+begin
+	select appointment_record(appointment_date_time, service_type, preferred_mechanic_id, license_plate_num) bulk collect into v_ret from appointment where customer_id = cid;
+	return(v_ret);
+end view_service_history;
+
+CREATE OR REPLACE FUNCTION
+schedule_maintainance_service(cid in int, lic in varchar, m in int, mech in varchar, d in timestamp)
+RETURN int
+AS
+mk varchar(100) ;
+md varchar(100);
+s_type varchar2(100);
+mech_id int;
+begin
+	select make, model into mk, md from vehicle where license_plate_num = lic;
+	select distinct max(service_type) into s_type from maintainance where vehicle_make = mk and vehicle_model = md and  miles <= m;
+	select employee_id  into mech_id from mechanic where name = mech;
+	insert into appointment values(d, s_type, null,  mech_id, lic, cid);
+	return(1);
+END schedule_maintainance_service;
+	
 
 
+/*________________________________________________sayali____________________________________________________*/
 
-
+/*1*/
 create or replace type profile_record as object (
 	name varchar(100),
 	address varchar(100),
@@ -226,7 +220,6 @@ create or replace type profile_record as object (
 
 create or replace type profile_table as table of profile_record;
 /
-
 
 create or replace function get_employee_profile(p_employee_id varchar)
 return profile_table as
@@ -264,4 +257,4 @@ end get_employee_profile;
 
 /*select *from table(get_employee_profile('634622236'));*/
 
->>>>>>> c4b4241d845c160b4a44a177f94289ca34d5b54c
+
