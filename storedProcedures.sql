e7f469aa5fc6bfe68abf51866e3dba1776b34d11
mrunmayi96 <mrunmayi96@gmail.com>
2018-11-19 20:12:31 -0500

    end of the day commit

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -51,14 +51,9 @@ AS
 x int := 0;
 begin 
 	select count(*) into x  from login_details where username=un and password = pw;
+	commit;
 	return(x);
 END authenticate_login;
-
-set serveroutput on
-begin
-dbms_ouput.put_line(authenticate_login('lovestory', 'lovestory123'));
-end;
-
 
 
 CREATE OR REPLACE FUNCTION
@@ -74,10 +69,11 @@ begin
 	if atype = 'manager' then
 		select name, address, phone_num, email into nm, ad, ph, em from manager where employee_id = userid;
 	elsif atype = 'receptionist' then
-		select name, address, phone_num, email into nm, ad, ph, em from manager where employee_id = userid;
+		select name, address, phone_num, email into nm, ad, ph, em from receptionist where employee_id = userid;
 	elsif atype = 'customer' then 
-		select name, address, phone_num, email into nm, ad, ph, em from manager where employee_id = userid;
+		select name, address, phone_num, email into nm, ad, ph, em from customer where customer_id = userid;
 	end if;
+	commit;
 	return(atype);
 END get_user_details;
 
@@ -93,6 +89,7 @@ begin
 	insert into customer values(cid, nm, ad, ph, em,  sc);
 	select count(*) into x from login_details where username = em;
 	select count(*) into y from customer where customer_id = cid;
+	commit;
 	if (x+y = 2) then
 		return(1);
 	else
@@ -128,7 +125,7 @@ begin
       owns, vehicle
     where
       p_customer_id = owns.customer_id and owns.license_plate_num = vehicle.license_plate_num;
-  
+    commit;
     return v_ret; 
   
 end get_vehicles;
@@ -146,6 +143,7 @@ begin
 	update customer set name = nm, address = ad, phone_num = ph where customer_id=cid;
 	update login_details set password = pw where id = cid;
 	x := 1;
+	commit;
 	return(x);
 end update_customer_profile;
 
@@ -161,6 +159,7 @@ begin
 	insert into owns values(lic, cid);
 	select count(*) into x from vehicle where license_plate_num = lic;
 	select count(*) into y from owns where license_plate_num = lic;
+	commit;
 	if (x+y = 2) then
 		return(1);
 	else
@@ -185,6 +184,7 @@ AS
 v_ret appointment_table;
 begin
 	select appointment_record(appointment_date_time, service_type, preferred_mechanic_id, license_plate_num) bulk collect into v_ret from appointment where customer_id = cid;
+	commit;
 	return(v_ret);
 end view_service_history;
 
@@ -202,6 +202,7 @@ begin
 	select distinct max(service_type) into s_type from maintainance where vehicle_make = mk and vehicle_model = md and  miles <= m;
 	select employee_id  into mech_id from mechanic where name = mech;
 	insert into appointment values(d, s_type, null,  mech_id, lic, cid);
+	commit;
 	return(1);
 END schedule_maintainance_service;
 	
@@ -214,24 +215,141 @@ mech_id int;
 begin
 	select employee_id  into mech_id from mechanic where name = mech;
 	insert into appointment values(d, prob, null,  mech_id, lic, cid);
+	commit;
 	return(1);
 END schedule_repair_service;
 
 /*9*/
 CREATE OR REPLACE type part_record as object(
-	part_name varchar(100);
+	part_name varchar2(100)
 );
-
+/
 CREATE OR REPLACE type part_table as table of part_record;
-
+/
 CREATE OR REPLACE FUNCTION
 diagnostic_report(prob in varchar)
 RETURN part_table
 AS
 v_ret part_table;
 begin
-	
+	select part_record(part_name) bulk collect into v_ret from basic_services bs, repair_services rs where rs.service_name = bs.service_name and rs.problem = prob;
+	commit;
+	return(v_ret);
 end diagnostic_report;
+/*select * from table(diagnostic_report('Engine knock'));*/	
+
+
+/*10*/
+CREATE OR REPLACE type part_price_record as object(
+	part_name varchar2(100),
+	price int
+);
+
+CREATE OR REPLACE type part_price_table as table of part_price_record;
+
+CREATE OR REPLACE FUNCTION
+get_service_parts_prices(dt in timestamp)
+RETURN part_price_table
+AS
+v_ret part_price_table;
+s_type varchar2(100);
+lic varchar2(100);
+mk varchar(100);
+md varchar(100);
+begin
+	select service_type, license_plate_num into s_type, lic from appointment where appointment_date_time=dt;
+	select make, model into mk, md from vehicle where license_plate_num = lic;
+	if s_type = 'A' then
+		select part_price_record(p.part_name, p.price) bulk collect into v_ret from basic_services bs, maintainance m, parts p where  m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'A' and bs.part_name = p.part_name and p.vehicle_make = m.vehicle_make;
+	elsif s_type = 'B' then
+		select part_price_record(p.part_name, p.price) bulk collect into v_ret from basic_services bs, maintainance m, parts p where  m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'B' and bs.part_name = p.part_name and p.vehicle_make = m.vehicle_make;
+	elsif s_type = 'C' then
+		select part_price_record(p.part_name, p.price) bulk collect into v_ret from basic_services bs, maintainance m, parts p where  m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'C' and bs.part_name = p.part_name and p.vehicle_make = m.vehicle_make;
+	else
+		select part_price_record(p.part_name, p.price) bulk collect into v_ret from basic_services bs, repair_services rs, parts p where  rs.service_name = bs.service_name and bs.part_name = p.part_name and p.vehicle_make = mk and rs.problem = s_type;
+	end if;
+	return(v_ret);
+	commit;
+end get_service_parts_prices;
+
+
+CREATE OR REPLACE FUNCTION
+get_detailed_invoice(dt in timestamp, lic out varchar, s_type out varchar, s_det out varchar, mech_name out varchar, cost out int, hours out float)
+RETURN int
+AS
+v_ret part_table;
+mech_id int;
+mech_wage int;
+mk varchar(100);
+md varchar(100);
+begin
+	select license_plate_num, service_type, preferred_mechanic_id into lic, s_det, mech_id from appointment where appointment_date_time = dt;
+	select name, hourly_salary into mech_name, mech_wage from mechanic where employee_id = mech_id;
+	select make, model into mk, md from vehicle where license_plate_num = lic;
+	if s_det = 'A' then  
+		s_type := 'maintainance';
+		select sum(bs.time) into hours from basic_services bs, repair_services rs, maintainance m where m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'A';
+		select sum(bs.charge_rate) into cost from basic_services bs, repair_services rs, maintainance m where m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'A';
+		cost := cost + (mech_wage * hours);
+	elsif s_det = 'B' then  
+		s_type := 'maintainance';
+		select sum(bs.time) into hours from basic_services bs, maintainance m where m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'B';
+		select sum(bs.charge_rate) into cost from basic_services bs, repair_services rs, maintainance m where m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'B';
+		cost := cost + (mech_wage * hours);
+	elsif s_det = 'C' then  
+		s_type := 'maintainance';
+		select sum(bs.time) into hours from basic_services bs, maintainance m where m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'C';
+		select sum(bs.charge_rate) into cost from basic_services bs, repair_services rs, maintainance m where m.vehicle_make = mk and m.vehicle_model = md and m.service_name = bs.service_name and m.service_type = 'C';
+		cost := cost + (mech_wage * hours);
+	else 
+		s_type := 'repair';
+		select sum(bs.time) into hours from basic_services bs, repair_services rs where rs.service_name = bs.service_name and rs.problem = s_det;
+		select sum(bs.charge_rate) into cost from basic_services bs, repair_services rs where rs.service_name = bs.service_name and rs.problem = s_det;
+		cost := cost + (mech_wage * hours);
+	end if; 
+	commit;
+	return(1);
+end get_detailed_invoice;
+
+/*select sum(bs.charge_rate) from basic_services bs, maintainance m where m.vehicle_make = 'Toyota' and m.vehicle_model = 'Corolla' and m.service_name = bs.service_name and m.service_type='A';*/
+
+/*11*/
+CREATE OR REPLACE FUNCTION
+get_upcoming_appointments(cid in int, lic out varchar, dt out timestamp, s_type out varchar, s_det out varchar, m_name out varchar)
+RETURN int
+AS
+mech_id int;
+x int:=1;
+begin
+	select license_plate_num, appointment_date_time, service_type, preferred_mechanic_id into lic, dt, s_det, mech_id from appointment where appointment_date_time > sysdate;
+	select name into m_name from mechanic where employee_id = mech_id;
+	if s_det = 'A' or s_det = 'B' or s_det = 'C' then
+		s_type := 'maintainance';
+	else
+		s_type := 'repair';
+	end if;
+	commit;
+	return(x);
+end get_upcoming_appointments;
+
+/*12*/
+CREATE OR REPLACE FUNCTION
+get_completed_appointments(cid in int, lic out varchar, dt out timestamp, s_type out varchar, s_det out varchar, m_name out varchar)
+RETURN int
+AS
+mech_id int;
+x int:=1;
+begin
+	select license_plate_num, appointment_date_time, service_type, preferred_mechanic_id into lic, dt, s_det, mech_id from appointment where appointment_date_time < sysdate;
+	select name into m_name from mechanic where employee_id = mech_id;
+	if s_det = 'A' or s_det = 'B' or s_det = 'C' then
+		s_type := 'maintainance';
+	else
+		s_type := 'repair';
+	end if;
+	commit;
+	return(x);
+end get_completed_appointments;
 
 /*________________________________________________sayali____________________________________________________*/
 
